b6627ac7881f5c73a7ca090c6fe51e8e8efff8d4
Vaibhav Patel <vaibhav0000patel@gmail.com>
2020-05-25 20:02:07 +0530

    Refactored : Unit test 2: Single Number

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -2,8 +2,9 @@ package main;
 
 public class StringCalculator {
 
-	public int Add(String numbers) {
-		if(numbers.length()==0) return 0; 
+	public int Add(String numbers) throws Exception {
+		if(numbers.length()==0) return 0;
+		if(numbers.length()==1) return Integer.parseInt(numbers);
 		return 9999;
 	}
 	
--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -23,43 +23,43 @@ public class StringCalculatorTest {
 	}
 
 	@Test
-	public void testAdd1() {
+	public void testAdd1() throws Exception {
 		// Empty String
 		assertTrue(cl.Add("")==0);
 	}
   
 	@Test
-	public void testAdd2() {
+	public void testAdd2() throws Exception {
 		// Single Number
 		assertTrue(cl.Add("1")==1);
 	}
 	
 	@Test
-	public void testAdd3() {
+	public void testAdd3() throws Exception {
 		// Two Numbers
 		assertTrue(cl.Add("1,2")==3);
 	}
 	
 	@Test
-	public void testAdd4() {
+	public void testAdd4() throws Exception {
 		// Handle unknown amount of numbers
 		assertTrue(cl.Add("1,2,3,4")==10);
 	}
 	
 	@Test
-	public void testAdd5() {
-		// Handle new new lines netween numbers
+	public void testAdd5() throws Exception {
+		// Handle new new lines between numbers
 		assertTrue(cl.Add("1\n2,3")==6);
 	}
 	
 	@Test
-	public void testAdd6() {
+	public void testAdd6() throws Exception {
 		// Invalid Input
 		assertTrue(cl.Add("1,\n")==-1);
 	}
 	
 	@Test
-	public void testAdd7() {
+	public void testAdd7() throws Exception {
 		// Support different delimiters 
 		assertTrue(cl.Add("//;\\n1;2")==3);
 	}
@@ -85,25 +85,25 @@ public class StringCalculatorTest {
 	}
 
 	@Test
-	public void testAdd10() {
+	public void testAdd10() throws Exception {
 		// Ignore number greater than 100
 		assertTrue(cl.Add("2,1001")==2);
 	}
 	
 	@Test
-	public void testAdd11() {
+	public void testAdd11() throws Exception {
 		// Support Variable length of delimiter 
 		assertTrue(cl.Add("//[***]\\n1***2***3")==6);
 	}
 	
 	@Test
-	public void testAdd12() {
+	public void testAdd12() throws Exception {
 		// Allow  multiple delimiters
 		assertTrue(cl.Add("//[*][%]\\n1*2%3")==6);
 	}
 	
 	@Test
-	public void testAdd13() {
+	public void testAdd13() throws Exception {
 		// Allow  multiple delimiters with variable length
 		assertTrue(cl.Add("//[****][%%%]\\\\n1****2%%%3")==6);
 	}
